d919d18f39080147bbee18836468bb6022068171
kinghyeongu <gusrns48@naver.com>
2022-02-08 16:51:43 +0900

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,83 @@
 import './App.css';
 import React from 'react';
-import $ from "jquery";
 import './style/css/index.css';
 
 class App extends React.Component {
-
-  componentDidMount() {
-    $('input').on('change', function () {
-      var v = $(this).val();
-      $('li[name=text]').css('font-size', v + 'px')
-      //$('div[name=fontname]').css('font-size', v / 3 + 'px')
-
-      if(v=='import_link'||v=='import_tag'){
-        v = $('.nowfontsize').text();
-      }
-      $('.nowfontsize').html(v)
-    }); 
-
-    $("li[name=text]").keydown(function(keyNum){ //fonts enter > detail popup_page
-      if(keyNum.keyCode == 13){
-        this.click()
-      }
-    });
-
-    $("li[name=text]").on("click", function(event) { //fonts click > detail popup_page
-      var fontname = this.className.replaceAll(" ","+") //web-font Ex code parameter fontname
-
-      $(".backon").show();
-      $(".backon").css("backdrop-filter", 'blur(4px)')
-
-      $(".popup").show(); //popup_page show
-
-      $("input[name=import_type]").each(function() { //radio reset
-        $(this).prop('checked', false);
-      });
-
-      $(".import_content").hide(); //Ex code reset
-      $('.import_code').hide();
-
-      $('.title').html(this.className) //popup title, content font change
-      $('.content').css('font-family',this.className)
-
-      $('#radio_btn').click(function () { //web-font Ex code change and show
-          var radioVal = $('input[name="import_type"]:checked').val(); //radio checked ?
-          if(radioVal == 'import_link'){
-            $('.import_code').html('&lt;link rel="preconnect" href="https://fonts.googleapis.com"&gt<br>')
-            $('.import_code').append('&lt;link rel="preconnect" href="https://fonts.gstatic.com" crossorigin&gt<br>')
-            $('.import_code').append(`&lt;link href=https://fonts.googleapis.com/css2?family=${fontname}&display=swap" rel="stylesheet"&gt;`)
-          }
-          else if(radioVal == 'import_tag'){
-            $('.import_code').html(`&lt;style&gt;@import url('https://fonts.googleapis.com/css2?family=${fontname}&display=swap');&lt;style&gt;`)
-          }
-          $('.import_code').show();
-      });
-    });
-
-    $("body").on("click", function(event) { //popup close
-      if(event.target.value == 'close' || event.target.className == 'backon'){
-          $(".popup").hide();
-          $(".backon").hide();
-      }
-    });
-
-    $(".import_title").keydown(function(keyNum){
-      if(keyNum.keyCode == 13){
-        this.click()
-      }
-    });
-
-    $(".import_title").on("click", function(event) { //web-font Ex code swich button
-      if($('.import_content').css('display') =='none'){
-        $('.import_content').show()
-      } else {
-        $('.import_content').hide()
-      }
-    });
-  }
-
   render(){
     return (
       <div> 
         <header>
           <div className='nowfontsize'>50</div><a> px</a>&ensp;
-          <input tabIndex="0" type="range" min="8" max="100" step="1" id="slider"/><hr/>
+          <input tabIndex="0" type="range" min="8" max="50" step="1" id="slider"/>
         </header>
         <main>
+          <section className="swiper mySwiper">
+            <ul className="swiper-wrapper">
+              <li name='text' id= 'Lobster Two' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Lobster Two'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Bangers' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Bangers'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Sacramento' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Sacramento'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Gloria Hallelujah' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Gloria Hallelujah'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Festive' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Festive'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Neucha' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Neucha'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Ole' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Ole'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Creepster' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Creepster'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Comforter' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Comforter'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Roboto' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Roboto'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Mochiy Pop P One' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Mochiy Pop P One'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Sedgwick Ave' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Sedgwick Ave'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Source Code Pro' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Source Code Pro'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Rubik Beastly' className="swiper-slide"><span className="fontText" style={{fontFamily: 'Rubik Beastly'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Neonderthaw' className="swiper-slide"><span  className="fontText" style={{fontFamily: 'Neonderthaw'}}>Almost before we knew it, we had left the ground.</span></li>
+              <li name='text' id= 'Pacifico' className="swiper-slide"><span  className="fontText" style={{fontFamily: 'Pacifico'}}>Almost before we knew it, we had left the ground.</span></li>
+            </ul>
+              <span className="swiper-pagination"></span>
+              <div className="swiper-button-next"></div>
+              <div className="swiper-button-prev"></div>
+              <div className="secondBT">
+                <input type="button" className="allshow" value="전체보기"/>
+              </div>
+            </section>
+
+          <article className="fontall">
+            <ul className="fontall_ul">
+              <li style={{fontFamily: "Lobster Two"}}>Lobster Two</li>
+              <li style={{fontFamily: "Bangers"}}>Bangers</li>
+              <li style={{fontFamily: "Sacramento"}}>Sacramento</li>
+              <li style={{fontFamily: "Gloria Hallelujah"}}>Gloria Hallelujah</li>
+              <li style={{fontFamily: "Festive"}}>Festive</li>
+              <li style={{fontFamily: "Neucha"}}>Neucha</li>
+              <li style={{fontFamily: "Ole"}}>Ole</li>
+              <li style={{fontFamily: "Creepster"}}>Creepster</li>
+              <li style={{fontFamily: "Comforter"}}>Comforter</li>
+              <li style={{fontFamily: "Roboto"}}>Roboto</li>
+              <li style={{fontFamily: "Mochiy Pop P One"}}>Mochiy Pop P One</li>
+              <li style={{fontFamily: "Sedgwick Ave"}}>Sedgwick Ave</li>
+              <li style={{fontFamily: "Source Code Pro"}}>Source Code Pro</li>
+              <li style={{fontFamily: "Rubik Beastly"}}>Rubik Beastly</li>
+              <li style={{fontFamily: "Neonderthaw"}}>Neonderthaw</li>
+              <li style={{fontFamily: "Pacifico"}}>Pacifico</li>
+            </ul>
+          </article>
+
           <section>
-            <ol className="fontbox">
-              <li tabIndex="0" name='text' className='Lobster Two'><div name='fontname' className='showfontname'>Lobster Two</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Bangers'><div name='fontname' className='showfontname'>Bangers</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Sacramento'><div name='fontname' className='showfontname'>Sacramento</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Gloria Hallelujah'><div name='fontname' className='showfontname'>Gloria Hallelujah</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Press Start 2P'><div name='fontname' className='showfontname'>Press Start 2P</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Neucha'><div name='fontname' className='showfontname'>Lobster</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Ole'><div name='fontname' className='showfontname'>Lobster</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Creepster'><div name='fontname' className='showfontname'>Creepster</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Comforter'><div name='fontname' className='showfontname'>Comforter</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Roboto'><div name='fontname' className='showfontname'>Roboto</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Mochiy Pop P One'><div name='fontname' className='showfontname'>Mochiy Pop P One</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Sedgwick Ave'><div name='fontname' className='showfontname'>Sedgwick Ave</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Source Code Pro'><div name='fontname' className='showfontname'>Source Code Pro</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Rubik Beastly'><div name='fontname' className='showfontname'>Rubik Beastly</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Neonderthaw'><div name='fontname' className='showfontname'>Neonderthaw</div>Almost before we knew it, we had left the ground.</li>
-              <li tabIndex="0" name='text' className='Pacifico'><div name='fontname' className='showfontname'>Pacifico</div>Almost before we knew it, we had left the ground.</li>
-            </ol>
+            <ul className="fontbox">
+              <li tabIndex="0" name='text' id="Lobster Two" className='Lobster Two'><div name='fontname' className='showfontname'>Lobster Two</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Bangers" className='Bangers'><div name='fontname' className='showfontname'>Bangers</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Sacramento" className='Sacramento'><div name='fontname' className='showfontname'>Sacramento</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Gloria Hallelujah" className='Gloria Hallelujah'><div name='fontname' className='showfontname'>Gloria Hallelujah</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Festive" className='Festive'><div name='fontname' className='showfontname'>Festive</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Neucha" className='Neucha'><div name='fontname' className='showfontname'>Neucha</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Ole" className='Ole'><div name='fontname' className='showfontname'>Ole</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Creepster" className='Creepster'><div name='fontname' className='showfontname'>Creepster</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Comforter" className='Comforter'><div name='fontname' className='showfontname'>Comforter</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Roboto" className='Roboto'><div name='fontname' className='showfontname'>Roboto</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Mochiy Pop P One" className='Mochiy Pop P One'><div name='fontname' className='showfontname'>Mochiy Pop P One</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Sedgwick Ave" className='Sedgwick Ave'><div name='fontname' className='showfontname'>Sedgwick Ave</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Source Code Pro" className='Source Code Pro'><div name='fontname' className='showfontname'>Source Code Pro</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Rubik Beastly" className='Rubik Beastly'><div name='fontname' className='showfontname'>Rubik Beastly</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Neonderthaw" className='Neonderthaw'><div name='fontname' className='showfontname'>Neonderthaw</div>Almost before we knew it, we had left the ground.</li>
+              <li tabIndex="0" name='text' id="Pacifico" className='Pacifico'><div name='fontname' className='showfontname'>Pacifico</div>Almost before we knew it, we had left the ground.</li>
+            </ul>
           </section>
 
           <article className="popup">
@@ -137,6 +111,21 @@ class App extends React.Component {
 
           <div className="backon"></div>
         </main>
+
+        <footer>
+          <div className="footer">
+            <div className="container">
+              <div className="first">
+                <div>소개</div>
+                <div>이용약관</div>
+                <div>개인정보취급방침</div>
+                <div>고객센터</div>
+              </div>
+              <div className="second">본 페이지는 Test페이지입니다.</div>
+              <div className="third">@2022 Onoffmix. All Rights Reserved</div>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
